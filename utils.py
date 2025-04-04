bb244b728f70769882a394a17650d5b4725b67da
zetatrap <reallzeta@outlook.es>
2025-04-04 14:53:26 -0300

    comitoso

--- utils.py
+++ utils.py
@@ -21,24 +21,24 @@ def generar_tiket():
         else:
             categorias_totales[categoria] = monto
 
-    # Obtener la fecha y hora actual
+
     fecha_hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # Abrir un cuadro de diálogo para elegir dónde guardar el archivo
+
     file_path = filedialog.asksaveasfilename(
         defaultextension=".txt",
         filetypes=[("Archivos de texto", "*.txt")],
         title="Guardar Ticket"
     )
 
-    # Si el usuario cancela el cuadro de diálogo, no hacer nada
+
     if not file_path:
         return
 
-    # Generar el archivo en la ubicación seleccionada
+    
     with open(file_path, "w", encoding='utf-8') as f:
         f.write("Tiket de Caja\n")
-        f.write(f"Fecha y Hora: {fecha_hora_actual}\n")  # Agregar fecha y hora
+        f.write(f"Fecha y Hora: {fecha_hora_actual}\n") 
         f.write("====================\n")
         for reg in registros:
             f.write(f"Tipo: {reg[1]}, Descripción: {reg[2]}, Categoría: {reg[3]}, Monto: {reg[4]}\n")
@@ -47,7 +47,7 @@ def generar_tiket():
         for cat, monto in categorias_totales.items():
             f.write(f"Total {cat}: {monto}\n")
 
-    # Borrar historial
+   
     cursor.execute("DELETE FROM caja")
     conn.commit()
     conn.close()
